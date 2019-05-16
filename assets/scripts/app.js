b8bcc7198a2f964d05cc5f7873bc3318e91efcde
DaveOrDavid <david.santore@gmail.com>
2019-05-16 10:43:40 -0400

    Complete auth api
    Tested and completed auth API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,5 +8,12 @@ const authEvents = require('./auth/events')
 
 $(() => {
   // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  console.log('sign UP submit button click for ' + authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn) // create onSignIn event
+  console.log('sign IN submit button click for ' + authEvents.onSignIn)
+  $('#change-pw').on('submit', authEvents.onChangePassword)
+  console.log('change pw submit button click for ' + authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  console.log('sign OUT submit button click for ' + authEvents.onSignOUT)
 })
