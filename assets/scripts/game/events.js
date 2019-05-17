a20164bba2df28aab481879e180046fcf6082db4
DaveOrDavid <david.santore@gmail.com>
2019-05-16 22:00:02 -0400

    Work on game logic for clicks
    Continued click game logic

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -8,6 +8,33 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 // 3) to update our webpage (and change what the end user sees)
 const ui = require('./ui.js')
+const cells = ["","","","","","","","",""]
+
+const onPlay = event => {
+  event.preventDefault()
+  const form = event.target
+  let player = function() {
+      if count($().on('click' event.target.gameboard)) === 0) {
+        ($().text('X', event.target))
+    } else if count($().on('click' event.target.gameboard)) % 3) {
+        ($().text('O', event.target))
+    } else if count($().on('click' event.target.gameboard)) % 2) {
+        ($().text('X', event.target))
+    } else
+  }
+}
+
+  for (let i = 1; i <= 100; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+    console.log('Fizzbuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else
+    console.log(i);
+  }
+
 
 const onCreate = event => { // event is the event, and then target is what we want the event to point to
   // if we submitted a form, this prevents the form from refreshing the page
@@ -32,6 +59,9 @@ const onCreate = event => { // event is the event, and then target is what we wa
 const onPlay = event => {
   event.preventDefault()
   const form = event.target
+
+
+
   console.log('Hi here is the ', form)
   console.log('you clicked the box')
   const formData = getFormFields(form)
@@ -39,43 +69,43 @@ const onPlay = event => {
     .then(ui.onCreateSuccess)
     .catch(ui.onCreateFailure)
 }
-
-const onIndex = event => { // event is the event, and then target is what we want the event to point to
-  event.preventDefault()
-  // const form = event.target
-  // const formData = getFormFields(form)  // no need for data, so remove input forms.
-  api.index()
-    .then(ui.onIndexSuccess)
-    .catch(ui.onIndexFailure)
-}
-
-const onShow = event => { // event is the event, and then target is what we want the event to point to
-  event.preventDefault()
-  const form = event.target
-  const formData = getFormFields(form)
-  api.show(formData)
-    .then(ui.onShowSuccess)
-    .catch(ui.onShowFailure)
-}
-
-const onDestroy = event => { // event is the event, and then target is what we want the event to point to
-  event.preventDefault()
-  const form = event.target
-  const formData = getFormFields(form)
-  api.destroy(formData)
-    .then(ui.onDestroySuccess)
-    .catch(ui.onDestroyFailure)
-}
-
-const onUpdate = event => { // event is the event, and then target is what we want the event to point to
-  event.preventDefault()
-  const form = event.target
-  console.log('Hi here is the ', form)
-  const formData = getFormFields(form)
-  api.update(formData)
-    .then(ui.onUpdateSuccess)
-    .catch(ui.onUpdateFailure)
-}
+//
+// const onIndex = event => { // event is the event, and then target is what we want the event to point to
+//   event.preventDefault()
+//   // const form = event.target
+//   // const formData = getFormFields(form)  // no need for data, so remove input forms.
+//   api.index()
+//     .then(ui.onIndexSuccess)
+//     .catch(ui.onIndexFailure)
+// }
+//
+// const onShow = event => { // event is the event, and then target is what we want the event to point to
+//   event.preventDefault()
+//   const form = event.target
+//   const formData = getFormFields(form)
+//   api.show(formData)
+//     .then(ui.onShowSuccess)
+//     .catch(ui.onShowFailure)
+// }
+//
+// const onDestroy = event => { // event is the event, and then target is what we want the event to point to
+//   event.preventDefault()
+//   const form = event.target
+//   const formData = getFormFields(form)
+//   api.destroy(formData)
+//     .then(ui.onDestroySuccess)
+//     .catch(ui.onDestroyFailure)
+// }
+//
+// const onUpdate = event => { // event is the event, and then target is what we want the event to point to
+//   event.preventDefault()
+//   const form = event.target
+//   console.log('Hi here is the ', form)
+//   const formData = getFormFields(form)
+//   api.update(formData)
+//     .then(ui.onUpdateSuccess)
+//     .catch(ui.onUpdateFailure)
+// }
 
 module.exports = {
   onCreate,
