628cdd329def4b7a9b74a09bb1ee675ed7432d13
DaveOrDavid <david.santore@gmail.com>
2019-05-20 22:31:31 -0400

    Added index games played, UI messages, console log removals
    Removed console logs and added UI message for games played. App logic
    for check for win refined.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,35 +3,26 @@
 const store = require('../store')
 
 const onSignUpSuccess = responseData => {
-  console.log('success', responseData)
   $('#message').text('Signed up successfully!')
   $('#message').removeClass()
   $('#message').addClass('success')
 }
 
 const onSignUpFailure = responseData => {
-  console.log('failure', responseData)
   $('#message').text('Signed up failed! :((')
   $('#message').removeClass()
   $('#message').addClass('failure')
 }
 
 const onSignInSuccess = responseData => {
-  console.log('success', responseData)
   $('#message').text('Signed in successfully!')
   $('#message').removeClass()
   $('#message').addClass('success')
 
-  // whenever we sign in scuccesfully, the user is returned in the response.
-  // So we store it, for any of our authenticated API calls.
-  // `responseData.user` contains `responseData.user.token`
-
   store.user = responseData.user
-  console.log('store is', store)
 }
 
 const onSignInFailure = responseData => {
-  console.log('failure', responseData)
   $('#message').text('Signed in failed! :((')
   $('#message').removeClass()
   $('#message').addClass('failure')
